5716b560328fc4ecad38569d950f7c5cf54d8b64
danbrmit <s4008809@student.rmit.edu.au>
2025-03-31 16:38:13 +1100

    added raycast functionality temp delete object for testing

---
+++ right_hand_controller.gd
@@ -0,0 +1,22 @@
+extends XRController3D
+var targetObject = null
+
+func _ready() -> void:
+	connect("button_pressed", btnPressedLeft)
+
+func _physics_process(delta: float) -> void:
+	if $RayCast3D.is_colliding():
+		var c = $RayCast3D.get_collider()
+		targetObject = c
+		#prints name of raycasted object
+		if targetObject:
+			print(targetObject.name)
+	else:
+		targetObject = null
+		
+
+func btnPressedLeft(name:String) -> void:
+	print(name)
+	if name == "trigger_click":
+		if targetObject:
+			targetObject.queue_free()
