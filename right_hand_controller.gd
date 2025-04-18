c620e76aadb5c7e4577ab0dd814aac48a093782f
danbrmit <s4008809@student.rmit.edu.au>
2025-04-18 23:11:06 +1000

    WIP dependency injection from main/options > player.tscn

--- right_hand_controller.gd
+++ right_hand_controller.gd
@@ -1,9 +1,27 @@
+#edited by: danb,
 extends XRController3D
 
-var targetObject = null
+#dependency injection TODO figure out how this works.
+var current_question = {}
 
-func _ready() -> void:
-	connect("button_pressed", btnPressedLeft)
+func set_question_data(q):
+	current_question = q
+
+var targetObject = null
+#
+#func _ready() -> void:
+	#connect("button_pressed", btnPressedLeft)
+var label_node: Label3D = null
+func set_label_node(label: Label3D):
+#from here
+	label_node = label
+
+
+func check_answer(selected: String):
+	var correct = current_question["answer"]
+	var main = get_parent()
+	var label = main.get_node("")
+	
 
 func _physics_process(delta: float) -> void:
 	if $RayCast3D.is_colliding():
@@ -14,10 +32,28 @@ func _physics_process(delta: float) -> void:
 			print(targetObject.name)
 	else:
 		targetObject = null
+	
+	if $RayCast3D.is_colliding() && Input.is_action_just_pressed("trigger_click"):
+		var hit = $RayCast3D.get_collider()
+		
+		#use this statement for any universal trigger click events, 
+		#and define which object meta depending on event context -danb
+		
+		#so for example, the if statement below will check if the object meta contains the "emotion" metadata.
+		#you could point the raycasting controller at something else, and it will not register, because of course
+		# it does not contain the "emotion" metadata, hence why this can be universally used.
+		# -danb
+	
+		if hit and hit.has_meta("emotion"):
+			var selected_emotion = hit.get_meta("emotion")
+			check_answer(selected_emotion)
+		
 		
 
-func btnPressedLeft(name:String) -> void:
-	print(name)
-	if name == "trigger_click":
-		if targetObject:
-			targetObject.queue_free()
+
+
+#func btnPressedLeft(name:String) -> void:
+	#print(name)
+	#if name == "trigger_click":
+		#if targetObject:
+			#targetObject.queue_free()
