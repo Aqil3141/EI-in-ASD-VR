90629f943c34e3140da269eda34d1134c93134e4
camomitch18 <s3971747@student.rmit.edu.au>
2025-05-06 00:41:15 +1000

    Website development

--- websiteScript.js
+++ websiteScript.js
@@ -1,4 +1,12 @@
-function toggleContent(header) {
-    const content = header.nextElementSibling;
-    content.style.display = content.style.display === "block" ? "none" : "block";
-}
+document.querySelectorAll('.header_scenarios').forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+
+        if (content.computedStyleMap.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            document.querySelectorAll('.data_content').forEach(c => c.style.display = 'none');
+            content.style.display = 'block';
+        }
+    });
+});
