39ab20b5f51fc6c57c39949f2818bf61613e3cba
camomitch18 <s3971747@student.rmit.edu.au>
2025-05-11 21:07:16 +1000

    Login Updated
    Added button to save anonymously, file name should change to something else

--- login.gd
+++ login.gd
@@ -1,25 +1,23 @@
 extends Control
 
-var email = ""
-var password
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+var username = ""
 
-var created = false
-
-func _on_login_button_down() -> void:
-	if !created:
-		email = $Email.text
-		password = $Password.text.sha256_text()
-		created = true
-		$Email.text = ""
-		$Password.text = ""
-		print("Account Created")
+# Saving data to database with inserted name
+func _on_save_name_button_down() -> void:
+	username = $VBoxContainer/Username.text
+	if username != "":
+		# Save data from all tutorial scenes to name as row in database
+		get_tree().change_scene_to_file("res://menu.tscn")
 	else:
-		if $Email.text == 	email:
-			if $Password.text.sha256_text() == password:
-				print("Login Success")
-				get_tree().change_scene_to_file("res://main.tscn")
-			else:
-				print("Login Fail")	
-
+		$VBoxContainer/Label.text = "Enter name"
+	
+# Saving data anonymously
+func _on_save_anon_button_down() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
+	
+# Back to menu
 func _on_back_button_down() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
