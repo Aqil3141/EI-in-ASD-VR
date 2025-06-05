9c97c4e17e6cb69e9024dc18827f52fb8fd257b0
Aqil Zaneefer <aqilzaneefer2004@gmail.com>
2025-06-05 13:52:44 +1000

    Audio added for mall

--- scripts/mall.gd
+++ scripts/mall.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var raycast := $XROrigin3D/RightHandController/RayCast3D
 @onready var label := $Label3D
+@onready var player := $AudioStreamPlayer
 
 var system_sequence = [tr("There's a young girl named Mariam standing nearby"), tr("Mariam appears to be lost Observe her closely"), tr("How do you think Mariam is feeling?"), "display", "continue", "move"]
 var girl_sequence = [tr("Mom? Dad? Where are you?"), tr("I... I can't find them...")] 
@@ -12,7 +13,7 @@ var cooldown_time := 0.5 # seconds
 var cooldown_timer := 0.0
 var movement = false;
 var emotion = false;
-
+var audio_stream;
 var mesh : MeshInstance3D = null;
 
 func _physics_process(delta):
@@ -35,12 +36,18 @@ func _physics_process(delta):
 		if collider.name == "Continue" and ($XROrigin3D/RightHandController.is_button_pressed("trigger") or $XROrigin3D/RightHandController.is_button_pressed("select_button")) and cooldown_timer <= 0 and !emotion:
 			if system_sequence[count%len(system_sequence)] == "continue":
 				label.text = tr("Press 'Continue' to move onto a different level")
+				audio_stream = load("res://assets/audio/System_mall_3.mp3")
+				player.stream = audio_stream
+				player.play()
 				$Continue/Label3D.text = tr("Continue")
 				$Continue/Label3D.font_size = 17
 			elif system_sequence[count%len(system_sequence)] == "move":
 				get_tree().change_scene_to_file("res://scenes/transition.tscn")
 			elif system_sequence[count%len(system_sequence)] != "display":
 				label.text = tr(system_sequence[count]) 
+				audio_stream = load("res://assets/audio/System_mall_"+str(count)+".mp3")
+				player.stream = audio_stream
+				player.play()
 			else:
 				display_emotions()
 			if $Continue/Label3D.text == tr("Continue"):
@@ -57,13 +64,19 @@ func _physics_process(delta):
 			if ($XROrigin3D/RightHandController.is_button_pressed("trigger") or $XROrigin3D/RightHandController.is_button_pressed("select_button")) and cooldown_timer <= 0:
 				if collider.name == "Happy":
 					label.text = tr("Mariam does not seem happy She is not smiling") 
+					audio_stream = load("res://assets/audio/Happy_mall.mp3")
 				if collider.name == "Sad":
 					label.text = tr("Mariam may seem sad but do you think she is feeling something else?")
+					audio_stream = load("res://assets/audio/Sad_mall.mp3")
 				if collider.name == "Fear":
 					label.text = tr("Correct Mariam is feeling fear Her hands are close to her body")
+					audio_stream = load("res://assets/audio/Fear_mall.mp3")
 					undisplay_emotions()
 				if collider.name == "Anger":
 					label.text = tr("Mariam does not seem angry Her eyebrows do not curl downwards")
+					audio_stream = load("res://assets/audio/Angry_mall.mp3")
+				player.stream = audio_stream
+				player.play()
 				start_cooldown()
 
 func start_cooldown():
